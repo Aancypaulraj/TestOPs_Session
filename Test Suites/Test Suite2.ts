22ec5134ec503bfff4f557000b535a83f766a322
aancy.p <aancy.p@inbl01lap7108.brd.g-c-i.com>
2023-06-15 10:20:28 +0530

    2nd commit

---
+++ Test Suites/Test Suite2.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>d1f32302-3f42-4713-acc7-3ddc4f7b7775</testSuiteGuid>
+</TestSuiteEntity>
